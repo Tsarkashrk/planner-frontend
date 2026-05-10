4d971f455327aa6add64c9410573bf6cb28f37d3
tsarkagmshrk <tokesh04@mail.ru>
2026-05-10 23:06:47 +0500

    add providers

--- src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -21,12 +21,7 @@ export function Sidebar() {
 						color={COLORS.primary}
 						size={38}
 					/>
-					<span className='text-2xl font-bold relative'>
-						RED Planner
-						<span className='absolute -top-1 -right-6 text-xs opacity-40 rotate-[18deg] font-normal'>
-							beta
-						</span>
-					</span>
+					<span className='text-2xl font-bold relative'>Planner</span>
 				</Link>
 				<div className='p-3 relative'>
 					<LogoutButton />
@@ -38,18 +33,6 @@ export function Sidebar() {
 					))}
 				</div>
 			</div>
-			<footer className='text-xs opacity-40 font-normal text-center p-layout'>
-				2024 &copy; With love from{' '}
-				<a
-					href='https://www.youtube.com/c/redgroup/?sub_confirmation=1'
-					target='_blank'
-					rel='noreferrer'
-					className='hover:text-primary text-brand-300 transition-colors'
-				>
-					RED Group
-				</a>
-				. <br /> All rights reserved.
-			</footer>
 		</aside>
 	)
 }
